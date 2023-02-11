613d95227499323b2b99c4a3c9b70d6159994b23
Ilona <ppsport88@mail.ru>
2023-02-11 20:16:25 +0300

    Task2

---
+++ Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+if (firstValue > secondValue)
+{
+    Console.WriteLine(firstValue + " = max ");
+}
+if (secondValue > firstValue)
+{
+    Console.WriteLine(secondValue + " = max ");
+}
