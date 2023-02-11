bd4a4dbc0f8a993f54021bd47c10e477ab8768a6
Ilona <ppsport88@mail.ru>
2023-02-11 20:42:00 +0300

    Task4

---
+++ Task04/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int thirdValue = Convert.ToInt32(Console.ReadLine());
+
+int max = firstValue;
+
+if (firstValue > max) max = firstValue;
+if (secondValue > max) max = secondValue;
+if (thirdValue > max) max = thirdValue;
+Console.Write(" max = ");
+Console.WriteLine(max);
