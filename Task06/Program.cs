6be4cdf415201f9fa4857a18dd1149675d0a4e75
Ilona <ppsport88@mail.ru>
2023-02-11 21:14:42 +0300

    Task6

---
+++ Task06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число " + number + " является чётным");
+}
+else
+{
+    Console.WriteLine("Число " + number + " является НЕчётным");
+}
